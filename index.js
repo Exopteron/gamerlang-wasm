07480615e697888c407f23ba27e525783436b22e
Exopteron <exopteron@protonmail.com>
2022-04-03 09:42:18 +1200

    tab

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ import { VMHolder } from "./pkg/pestwasm.js";
 function run() {
     const output = document.getElementById("terminal");
     const input = document.getElementById("input");
+    input.addEventListener('keydown', (event) => {
+        if (e.key == 'Tab') {
+            e.preventDefault();
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+        
+            // set textarea value to: text before caret + tab + text after caret
+            this.value = this.value.substring(0, start) +
+              "\t" + this.value.substring(end);
+        
+            // put caret at right position again
+            this.selectionStart =
+              this.selectionEnd = start + 1;
+          }
+    });
     const vm = new VMHolder();
     input.addEventListener('input', (event) => {
         output.textContent = vm.run(event.target.value);
