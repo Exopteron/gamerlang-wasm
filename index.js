524db353b48218e89f53ba8638cfce73774fc6e6
Exopteron <exopteron@protonmail.com>
2022-04-03 09:29:08 +1200

    index

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import init from "./pkg/pestwasm.js";
 import { VMHolder } from "./pkg/pestwasm.js";
+function run() {
+    const output = document.getElementById("terminal");
+    const input = document.getElementById("input");
+    const vm = new VMHolder();
+    input.addEventListener('input', (event) => {
+        output.textContent = vm.run(event.target.value);
+    });
+}
 
-const output = document.getElementById("terminal");
-const input = document.getElementById("input");
-const vm = new VMHolder();
-input.addEventListener('input', (event) => {
-    output.textContent = vm.run(event.target.value);
-});
+init().then(run)
